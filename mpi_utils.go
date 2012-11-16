42192836f72a83cfc143ff1e85f70cfe985951b3
Thierfelder Marcus <mthierfe@oflws204.dwd.de>
2012-11-16 18:09:08 +0100

    redirect stderr too

--- mpi_utils.go
+++ mpi_utils.go
@@ -9,6 +9,7 @@ package mpi
 
 void call_freopen(char *file) {
 	freopen(file, "w", stdout);
+	freopen(file, "w", stderr);
 }
 
 */
@@ -18,8 +19,9 @@ import (
 	"fmt"
 )
 
-/* if you want to redirect each stdout per process in a file 
-   in order to avoid a parallel stdout mess */
+
+// if you want to redirect each stdout per process in a file 
+// in order to avoid a parallel stdout mess (no standard mpi function)
 func Redirect_STDOUT(comm C.MPI_Comm) {
 	rank := Comm_rank(comm)
 	file := fmt.Sprintf("stdout.%04d", rank)
